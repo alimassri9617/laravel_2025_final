2c37d48e7bf50798f30df55c53ce54483363a06c
hassan <202212004@ua.edu.lb>
2025-04-24 13:41:55 +0300

    otp

--- config/mail.php
+++ config/mail.php
@@ -16,6 +16,24 @@ return [
 
     'default' => env('MAIL_MAILER', 'log'),
 
+    // Added log mailer for testing
+    'mailers' => [
+        'smtp' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'auth_mode' => null,
+        ],
+        'log' => [
+            'transport' => 'log',
+            'channel' => env('MAIL_LOG_CHANNEL'),
+        ],
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Mailer Configurations
